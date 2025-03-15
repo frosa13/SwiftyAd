a4c2265d93da52392eabe2471d10482c283121e7
Francisco Rosa <franciscorosa1309@gmail.com>
2025-03-15 09:40:11 +0000

    Initial Commit

---
+++ SwiftyAd/SwiftyAdApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyAdApp.swift
+//  SwiftyAd
+//
+//  Created by Francisco Rosa on 15/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyAdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
